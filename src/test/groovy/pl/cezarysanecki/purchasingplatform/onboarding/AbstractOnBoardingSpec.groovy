b0b38fa0b6b226cc74d94fbdf37669631f117e2a
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-11 18:10:53 +0200

    prepared test with config

--- src/test/groovy/pl/cezarysanecki/purchasingplatform/onboarding/AbstractOnBoardingSpec.groovy
+++ src/test/groovy/pl/cezarysanecki/purchasingplatform/onboarding/AbstractOnBoardingSpec.groovy
@@ -1,7 +1,13 @@
-package pl.cezarysanecki.purchasingplatform
+package pl.cezarysanecki.purchasingplatform.onboarding
 
+import pl.cezarysanecki.purchasingplatform.shared.events.DomainEventPublisher
 import spock.lang.Specification
 
-abstract class AbstractSpec extends Specification {
-
+abstract class AbstractOnBoardingSpec extends Specification {
+  
+  DomainEventPublisher publisher = Mock(DomainEventPublisher)
+  
+  LeadingConfig leadingConfig = LeadingConfig.forTests(publisher)
+  LeadingFacade leadingFacade = leadingConfig.leadingFacade()
+  
 }
--- src/test/groovy/pl/cezarysanecki/purchasingplatform/onboarding/RegisteringRegistrationFormSpec.groovy
+++ src/test/groovy/pl/cezarysanecki/purchasingplatform/onboarding/RegisteringRegistrationFormSpec.groovy
@@ -1,19 +1,38 @@
 package pl.cezarysanecki.purchasingplatform.onboarding
 
-import pl.cezarysanecki.purchasingplatform.onboarding.AbstractOnBoardingSpec
+import pl.cezarysanecki.purchasingplatform.onboarding.api.RegistrationFormId
+import pl.cezarysanecki.purchasingplatform.onboarding.api.SellerRegistrationForm
+import pl.cezarysanecki.purchasingplatform.shared.model.*
+import spock.lang.Subject
 
-class RegisteringRegistartionFormSpec extends AbstractOnBoardingSpec {
-
-    def "simple test"() {
-        given:
-        int a = 2
-        int b = 3
-
-        when:
-        int result = a + b
-
-        then:
-        result == 5
-    }
-
+class RegisteringRegistrationFormSpec extends AbstractOnBoardingSpec {
+  
+  @Subject
+  LeadingFacade sut = leadingFacade
+  
+  def "should register seller form"() {
+    given:
+      SellerRegistrationForm registrationForm = SellerRegistrationForm.builder()
+          .companyName(CompanyName.of("Test"))
+          .regon(Regon.of("123456789"))
+          .address(
+              Address.builder()
+                  .street("Test Street")
+                  .houseNumber(12)
+                  .city("Test City")
+                  .zipCode("01-555")
+                  .build())
+          .email(Email.of("test@test.pl"))
+          .phoneNumber(PhoneNumber.of("123456789"))
+          .build()
+    
+    when:
+      RegistrationFormId registrationFormId = sut.register(registrationForm)
+    and:
+      SellerRegistrationForm result = sut.show(registrationForm)
+    
+    then:
+      result == registrationForm
+  }
+  
 }
