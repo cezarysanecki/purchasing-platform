55e26489d819467c39eac30fbe6081db8462eb3b
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-11 23:14:40 +0200

    used facade in handler for saving repo

--- src/main/java/pl/cezarysanecki/purchasingplatform/verification/infrastructure/VerificationEventListener.java
+++ src/main/java/pl/cezarysanecki/purchasingplatform/verification/infrastructure/VerificationEventListener.java
@@ -6,14 +6,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import pl.cezarysanecki.purchasingplatform.onboarding.api.LeadingEvent;
+import pl.cezarysanecki.purchasingplatform.verification.VerificationFacade;
 
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class VerificationEventListener {
 
+  private final VerificationFacade verificationFacade;
+
   @EventListener
   public void handle(@NonNull LeadingEvent.RegistrationFormRegistered event) {
-
+    verificationFacade.saveNew(event.getRegistrationForm());
   }
 
 }
