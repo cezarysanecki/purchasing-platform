8667ae95796a0db28464425fa84b60e428216306
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-11 17:13:04 +0200

    prepared repo with return id

---
+++ src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/LeadingRepository.java
@@ -0,0 +1,10 @@
+package pl.cezarysanecki.purchasingplatform.onboarding;
+
+import pl.cezarysanecki.purchasingplatform.onboarding.dto.RegistrationFormId;
+import pl.cezarysanecki.purchasingplatform.onboarding.dto.SellerRegistrationForm;
+
+interface LeadingRepository {
+
+  RegistrationFormId save(final SellerRegistrationForm sellerRegistrationForm);
+
+}
---
+++ src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/dto/RegistrationFormId.java
@@ -0,0 +1,9 @@
+package pl.cezarysanecki.purchasingplatform.onboarding.dto;
+
+import lombok.NonNull;
+
+import java.util.UUID;
+
+public record RegistrationFormId(@NonNull UUID id) {
+
+}
