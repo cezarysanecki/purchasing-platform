31a1df9a423e8cb0b372b80c0cd4273ef3455a13
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-11 18:15:31 +0200

    added behaviour test

--- src/test/groovy/pl/cezarysanecki/purchasingplatform/onboarding/RegisteringRegistrationFormSpec.groovy
+++ src/test/groovy/pl/cezarysanecki/purchasingplatform/onboarding/RegisteringRegistrationFormSpec.groovy
@@ -1,5 +1,6 @@
 package pl.cezarysanecki.purchasingplatform.onboarding
 
+import pl.cezarysanecki.purchasingplatform.onboarding.api.LeadingEvent
 import pl.cezarysanecki.purchasingplatform.onboarding.api.RegistrationFormId
 import pl.cezarysanecki.purchasingplatform.onboarding.api.SellerRegistrationForm
 import pl.cezarysanecki.purchasingplatform.shared.model.*
@@ -33,6 +34,29 @@ class RegisteringRegistrationFormSpec extends AbstractOnBoardingSpec {
     
     then:
       result == registrationForm
+  }
+  
+  def "should inform that registration form is registered"() {
+    given:
+      SellerRegistrationForm registrationForm = SellerRegistrationForm.builder()
+          .companyName(CompanyName.of("Test"))
+          .regon(Regon.of("123456789"))
+          .address(
+              Address.builder()
+                  .street(Address.Street.of("Test Street"))
+                  .houseNumber(Address.HouseNumber.of(12))
+                  .postalCode(Address.PostalCode.of("01-555"))
+                  .city(Address.City.of("Test City"))
+                  .build())
+          .email(Email.of("test@test.pl"))
+          .phoneNumber(PhoneNumber.of("123456789"))
+          .build()
+    
+    when:
+      sut.register(registrationForm)
+    
+    then:
+      1 * publisher.publish(_ as LeadingEvent.RegistrationFormRegistered)
   }
   
 }
