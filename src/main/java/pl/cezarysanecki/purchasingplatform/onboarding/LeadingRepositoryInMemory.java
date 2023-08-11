29cf19bc8ee41f9e14c007e61a6363932c1dfa07
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-11 17:26:28 +0200

    made in memory bean for dev env

--- src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/LeadingRepositoryInMemory.java
+++ src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/LeadingRepositoryInMemory.java
@@ -1,14 +1,18 @@
 package pl.cezarysanecki.purchasingplatform.onboarding;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import pl.cezarysanecki.purchasingplatform.onboarding.dto.RegistrationFormId;
 import pl.cezarysanecki.purchasingplatform.onboarding.dto.SellerRegistrationForm;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+@Component
+@Profile("dev")
 class LeadingRepositoryInMemory implements LeadingRepository {
 
-  private final Map<RegistrationFormId, SellerRegistrationForm> database = new HashMap<>();
+  private final Map<RegistrationFormId, SellerRegistrationForm> database = new ConcurrentHashMap<>();
 
   @Override
   public RegistrationFormId save(final SellerRegistrationForm sellerRegistrationForm) {
