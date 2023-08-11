ec6ae76d083658843058225aa6abc8ebf6906139
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-11 17:55:03 +0200

    published event when regitration form registered

--- src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/LeadingConfig.java
+++ src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/LeadingConfig.java
@@ -4,20 +4,22 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.cezarysanecki.purchasingplatform.shared.events.DomainEventPublisher;
 
 @Configuration
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class LeadingConfig {
 
   private final LeadingRepository leadingRepository;
+  private final DomainEventPublisher publisher;
 
-  public static LeadingConfig forTests() {
-    return new LeadingConfig(new LeadingRepositoryInMemory());
+  public static LeadingConfig forTests(final DomainEventPublisher publisher) {
+    return new LeadingConfig(new LeadingRepositoryInMemory(), publisher);
   }
 
   @Bean
   LeadingFacade leadingFacade() {
-    return new LeadingFacade(leadingRepository);
+    return new LeadingFacade(leadingRepository, publisher);
   }
 
 }
--- src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/LeadingFacade.java
+++ src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/LeadingFacade.java
@@ -3,17 +3,22 @@ package pl.cezarysanecki.purchasingplatform.onboarding;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import pl.cezarysanecki.purchasingplatform.onboarding.api.LeadingEvent;
 import pl.cezarysanecki.purchasingplatform.onboarding.api.RegistrationFormId;
 import pl.cezarysanecki.purchasingplatform.onboarding.api.SellerRegistrationForm;
+import pl.cezarysanecki.purchasingplatform.shared.events.DomainEventPublisher;
 
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public class LeadingFacade {
 
   private final LeadingRepository leadingRepository;
+  private final DomainEventPublisher publisher;
 
   @Transactional
   public RegistrationFormId register(final SellerRegistrationForm sellerRegistrationForm) {
-    return leadingRepository.save(sellerRegistrationForm);
+    RegistrationFormId registrationFormId = leadingRepository.save(sellerRegistrationForm);
+    publisher.publish(LeadingEvent.RegistrationFormRegistered.now(registrationFormId));
+    return registrationFormId;
   }
 
   @Transactional(readOnly = true)
