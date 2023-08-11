43b4a512e085b60001cba7b2ed913e08392d7b79
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-11 21:17:11 +0200

    extracted sample form

--- src/test/groovy/pl/cezarysanecki/purchasingplatform/onboarding/RegisteringRegistrationFormSpec.groovy
+++ src/test/groovy/pl/cezarysanecki/purchasingplatform/onboarding/RegisteringRegistrationFormSpec.groovy
@@ -13,19 +13,7 @@ class RegisteringRegistrationFormSpec extends AbstractOnBoardingSpec {
   
   def "should register seller form"() {
     given:
-      SellerRegistrationForm registrationForm = SellerRegistrationForm.builder()
-          .companyName(CompanyName.of("Test"))
-          .regon(Regon.of("123456789"))
-          .address(
-              Address.builder()
-                  .street(Address.Street.of("Test Street"))
-                  .houseNumber(Address.HouseNumber.of(12))
-                  .postalCode(Address.PostalCode.of("01-555"))
-                  .city(Address.City.of("Test City"))
-                  .build())
-          .email(Email.of("test@test.pl"))
-          .phoneNumber(PhoneNumber.of("123456789"))
-          .build()
+      SellerRegistrationForm registrationForm = sellerRegistrationForm()
     
     when:
       RegistrationFormId registrationFormId = sut.register(registrationForm)
@@ -38,25 +26,29 @@ class RegisteringRegistrationFormSpec extends AbstractOnBoardingSpec {
   
   def "should inform that registration form is registered"() {
     given:
-      SellerRegistrationForm registrationForm = SellerRegistrationForm.builder()
-          .companyName(CompanyName.of("Test"))
-          .regon(Regon.of("123456789"))
-          .address(
-              Address.builder()
-                  .street(Address.Street.of("Test Street"))
-                  .houseNumber(Address.HouseNumber.of(12))
-                  .postalCode(Address.PostalCode.of("01-555"))
-                  .city(Address.City.of("Test City"))
-                  .build())
-          .email(Email.of("test@test.pl"))
-          .phoneNumber(PhoneNumber.of("123456789"))
-          .build()
+      SellerRegistrationForm registrationForm = sellerRegistrationForm()
     
     when:
       sut.register(registrationForm)
     
     then:
       1 * publisher.publish(_ as LeadingEvent.RegistrationFormRegistered)
+  }
+  
+  private SellerRegistrationForm sellerRegistrationForm() {
+    return SellerRegistrationForm.builder()
+        .companyName(CompanyName.of("Test"))
+        .regon(Regon.of("123456789"))
+        .address(
+            Address.builder()
+                .street(Address.Street.of("Test Street"))
+                .houseNumber(Address.HouseNumber.of(12))
+                .postalCode(Address.PostalCode.of("01-555"))
+                .city(Address.City.of("Test City"))
+                .build())
+        .email(Email.of("test@test.pl"))
+        .phoneNumber(PhoneNumber.of("123456789"))
+        .build()
   }
   
 }
