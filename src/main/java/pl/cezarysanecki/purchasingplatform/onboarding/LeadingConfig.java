8b2d6d1058f168ef7175f37e9c172919dbd19566
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-11 17:17:00 +0200

    prepared leading repo for dev profile

--- src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/LeadingConfig.java
+++ src/main/java/pl/cezarysanecki/purchasingplatform/onboarding/LeadingConfig.java
@@ -3,12 +3,14 @@ package pl.cezarysanecki.purchasingplatform.onboarding;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @RequiredArgsConstructor
 class LeadingConfig {
 
   @Bean
+  @Profile("dev")
   LeadingRepository leadingRepository() {
     return new LeadingRepositoryInMemory();
   }
