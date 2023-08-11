ca481a9c63766b13b08716baf4a21bd2a156c73a
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-11 17:05:05 +0200

    added phone number vo

---
+++ src/main/java/pl/cezarysanecki/purchasingplatform/shared/PhoneNumber.java
@@ -0,0 +1,24 @@
+package pl.cezarysanecki.purchasingplatform.shared;
+
+import lombok.NonNull;
+import lombok.Value;
+
+@Value
+public class PhoneNumber {
+
+  @NonNull
+  String phoneNumber;
+
+  public PhoneNumber(final String phoneNumber) {
+    if (phoneNumber.length() != 9) {
+      throw new IllegalArgumentException("Phone number must be 9 digits long");
+    }
+    this.phoneNumber = phoneNumber;
+  }
+
+  @Override
+  public String toString() {
+    return phoneNumber;
+  }
+
+}
